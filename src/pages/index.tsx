73f0187cea5e8320015dde9e735d3e48ffee12a7
Lazaro1 <lazarolimasilva2@gmail.com>
2021-08-18 16:59:35 -0300

    config project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'lázaro' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">{title}!</a>
         </h1>
 
         <p className={styles.description}>
