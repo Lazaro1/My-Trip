6609b60ada7593433ebeca30d51cc541b503420e
Lazaro lima <lazaro@pop-os.localdomain>
2021-10-16 18:31:21 -0300

    boilerplate before PWA

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScrip, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
