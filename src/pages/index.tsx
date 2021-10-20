7f8d720b4a80d6ec98c69463c7874e2558aa32f7
Lazaro lima <lazaro@pop-os.localdomain>
2021-10-20 20:12:55 -0300

    correct branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-
-import React from "react"
-import Login from 'pages/index'
-import UserContext from 'contexts/UserContex';
+import React from 'react'
+import Login from 'pages/Login'
+import UserContext from 'contexts/UserContex'
 
 export default function Home() {
-  
-  return <UserContext>
-    <Login />
-  </UserContext>
+  return (
+    <UserContext>
+      <Login />
+    </UserContext>
+  )
 }
