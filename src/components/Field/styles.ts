13041b52febe695d371ed43989d889655c59bd6d
Lazaro1 <lazarolimasilva2@gmail.com>
2021-11-20 15:54:46 -0300

    formik and yup

---
+++ src/components/Field/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const InputZone = styled.div`
+  width: 87%;
+  height: 70px;
+  margin-bottom: 5px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  background: #ffffff;
+  border: 2px solid #c1bbbb;
+
+  &:hover {
+    border-left: 4px solid blue;
+  }
+`
+
+export const InputLabel = styled.label`
+  /* position: absolute; */
+  margin: 12px 0px 6px 27px;
+  margin-left: 27px;
+  font-size: 18px;
+  font-weight: 400;
+  font-family: 'Roboto', sans-serif;
+  line-height: 21px;
+
+  color: rgba(0, 0, 0, 0.61);
+`
+
+export const Input = styled.input`
+  outline: none;
+  border: none;
+  margin: 0px 0px 6px 27px;
+
+  font-size: 18px;
+  line-height: 21px;
+  font-weight: 500;
+  line-height: 21.09px;
+  font-family: 'Roboto', sans-serif;
+  color: #3751fe;
+`
